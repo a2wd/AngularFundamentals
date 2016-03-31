e5f02724d9deb8bb83f735a8c52f91d371623659
a2wd <github@a2wd.com>
2016-03-31 08:17:40 +0100

    Directives with shared controller

--- app/js/directives/greeting.js
+++ app/js/directives/greeting.js
@@ -5,18 +5,32 @@ eventsApp.directive("greeting", function(){
     restrict: "E",
     replace: true,
     template: "<button class='btn' ng-click='sayHello()'>Say hello</button>",
-    //Controller could be definde inline
-    //Or could refer to a separate controler
-    //controller: "GreetingController"
-
-    //Or, we can define it in the element
-    controller: "@",
-    name: "ctrl"
+    controller: function($scope) {
+      var greetings = ["hello"]
+      $scope.sayHello = function() {
+        console.log(greetings.join(", ") + "!")
+      }
+      this.addGreeting = function(greeting) {
+        greetings.push(greeting)
+      }
+    }
   }
 })
-
-eventsApp.controller("GreetingController", function($scope){
-  $scope.sayHello = function() {
-    console.log("hello")
+.directive("spanish", function(){
+  return {
+    restrict: "A",
+    require: "greeting",
+    link: function(scope, element, attr, controller) {
+      controller.addGreeting("Buendia")
+    }
+  }
+})
+.directive("hebrew", function(){
+  return {
+    restrict: "A",
+    require:"greeting",
+    link: function(scope, element, attr, controller) {
+      controller.addGreeting("שָׁלוֹם")
+    }
   }
 })
