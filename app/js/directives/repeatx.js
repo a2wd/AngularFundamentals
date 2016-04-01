554e14a0c7a75f1ac7d5264db0e00c7bc25217eb
a2wd <github@a2wd.com>
2016-04-01 17:18:32 +0100

    Example with compiled dom

--- app/js/directives/repeatx.js
+++ app/js/directives/repeatx.js
@@ -1,10 +1,10 @@
 "use strict";
 
-eventsApp.directive("repeatX", function(){
+eventsApp.directive("repeatX", function($compile){
   return {
     link: function(scope, element, attributes, controller) {
       for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
-        element.after(element.clone().attr("repeat-x", 0))
+        element.after($compile(element.clone().attr("repeat-x", 0))(scope))
       }
     }
   }
