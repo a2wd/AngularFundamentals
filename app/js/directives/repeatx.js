7f52ad2d9b3cc48f1170349d0d8d73bc23ede6ba
a2wd <github@a2wd.com>
2016-04-01 17:27:23 +0100

    Example with compile function and joined linking function

--- app/js/directives/repeatx.js
+++ app/js/directives/repeatx.js
@@ -6,6 +6,13 @@ eventsApp.directive("repeatX", function(){
       for (var i = 0; i < Number(attributes.repeatX) - 1; i++) {
         element.after(element.clone().attr("repeat-x", 0))
       }
+      return function(scope, element, attributes, controller) {
+        attributes.$observe("text", function(newValue){
+          if (newValue === "Hello World") {
+            element.css("color", "red")
+          }
+        })
+      }
     }
   }
 })
